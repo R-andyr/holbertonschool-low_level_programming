7641c943fb6fc3da4f781841c19d80aa846d9413
R-andyr <tyraeljacob@gmail.com>
2022-02-11 12:12:08 -0800

    Included libraries

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
  * main - Evaluates a number
@@ -8,6 +10,9 @@
 int main(void)
 {
 	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 		printf("%d is positive\n", n);
