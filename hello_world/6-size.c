ce2ddffc027b58d8dd89759f33abe289eefb40eb
R-andyr <tyraeljacob@gmail.com>
2022-06-13 17:28:55 -0700

    fix desired output

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	printf("Size of a char: %ld bytes\n", sizeof(char));
-	printf("Sizeprint of an int: %ld bytes\n", sizeof(int));
+	printf("Size of an int: %ld bytes\n", sizeof(int));
 	printf("Size of a long int: %ld bytes\n", sizeof(long));
 	printf("Size of a float: %ld bytes\n", sizeof(float));
 return (0);
