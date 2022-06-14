b7eb94c123f8ffe4db5bd23759ad5b237c764f8f
R-andyr <tyraeljacob@gmail.com>
2022-06-13 17:41:35 -0700

    add missing data type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	printf("Size of a char: %ld bytes\n", sizeof(char));
 	printf("Size of an int: %ld bytes\n", sizeof(int));
 	printf("Size of a long int: %ld bytes\n", sizeof(long));
+	printf("Size of a long long int: %1w bytes\n", sizeof(long long));
 	printf("Size of a float: %ld bytes\n", sizeof(float));
 return (0);
 }
