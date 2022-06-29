1f7e040619c207606cb37c357a7625dbefa9eefd
R-andyr <tyraeljacob@gmail.com>
2022-06-29 15:48:29 -0700

    :3

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,22 +2,22 @@
 
 /**
  * _strcat - concatenates two strings.
- * @dest: string
- * @src: string
+ * @dest: first string
+ * @src: second string
  * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-	int count, count2;
+	int c, c2;
 
-	for (count = 0; dest[count] != '\0'; count++)
+	for (c = 0; dest[c] != '\0'; c++)
 	{
 	}
-	for (count2 = 0; src[count2] != '\0'; count2++)
+	for (c2 = 0; src[c2] != '\0'; c2++)
 	{
-		dest[count] = src[count2];
-		count++;
+		dest[c] = src[c2];
+		c++;
 	}
-	dest[count] = '\0';
+	dest[c] = '\0';
 	return (dest);
 }
