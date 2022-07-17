f1971ea3555aa899f5fbd07bfcb06fb618c17a80
R-andyr <tyraeljacob@gmail.com>
2022-07-16 22:14:54 -0700

    3. To be is a to be the value of a variable

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,29 +11,41 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	char *sep = "";
+	va_list ap;
 	int i = 0;
+	char *s;
 
-	va_start(valist, format);
 	while (format && format[i])
 	{
-		if (I > 0 && (format[i] == 'c' || format[i] == 'i' 
-					|| format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-		switch (format[i])
+		va_start(ap, format);
+		while (format[i])
 		{
-		case 'c':
-			printf("%c", va_arg(valist, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(valist, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(valist, double));
-			break;
-		case 's':
-			if (va_arg(valist, char *) == NULL)
-		}
+			switch (format[i++])
+			{
+				case 'c':
+					printf("%c", (char) va_arg(ap, int));
+					break;
+				case 'i':
+					printf("%i", va_arg(ap, int));
+					break;
+				case 'f':
+					printf("%f", (float) va_arg(ap, double));
+					break;
+				case 's':
+					s = va_arg(ap, char *);
+					if (s)
+						printf("%s", s);
+					else
+						printf("(nil)");
+					break;
+				default:
+					break;
+			}
+			if (format[i] && (format[i] == 'c' || format[i] == 'i' || format[i]))
+				printf(", ");
+		i++;
 	}
+	va_end(ap);
+	}
+	printf("\n");
 }
